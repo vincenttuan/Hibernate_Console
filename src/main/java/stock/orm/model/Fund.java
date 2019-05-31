faf81877676ce56c4a228ea1c3bbf16769b0f684
student <student@192.168.2.15>
2019-05-31 19:53:21 +0800

    JavaWeb

---
+++ src/main/java/stock/orm/model/Fund.java
@@ -0,0 +1,53 @@
+package stock.orm.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Fund {
+
+    private Integer fundId;
+    private String name;
+    private String desc;
+    private Set<Stock> stocks = new HashSet<>();
+    
+    public Fund() {
+    }
+    
+    public Fund(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public Integer getFundId() {
+        return fundId;
+    }
+
+    public void setFundId(Integer fundId) {
+        this.fundId = fundId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Set<Stock> getStocks() {
+        return stocks;
+    }
+
+    public void setStocks(Set<Stock> stocks) {
+        this.stocks = stocks;
+    }
+
+}
---
+++ src/main/java/stock/orm/model/Stock.java
@@ -0,0 +1,38 @@
+package stock.orm.model;
+
+public class Stock implements java.io.Serializable {
+
+    private Integer stockId;
+    private String stockCode;
+    private String stockName;
+    
+    public Stock() {
+        
+    }
+
+    public Integer getStockId() {
+        return stockId;
+    }
+
+    public void setStockId(Integer stockId) {
+        this.stockId = stockId;
+    }
+
+    public String getStockCode() {
+        return stockCode;
+    }
+
+    public void setStockCode(String stockCode) {
+        this.stockCode = stockCode;
+    }
+
+    public String getStockName() {
+        return stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+    
+    
+}
