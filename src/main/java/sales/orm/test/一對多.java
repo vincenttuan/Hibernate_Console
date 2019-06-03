66b7820c537fd6ce903499f99c8ecfc78882871c
student <student@192.168.2.15>
2019-06-03 21:19:42 +0800

    JavaWeb

---
+++ src/main/java/sales/orm/test/一對多.java
@@ -0,0 +1,46 @@
+package sales.orm.test;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import sales.orm.model.Customer;
+import sales.orm.model.Sales;
+
+public class 一對多 {
+
+    public static void main(String[] args) {
+        create();
+
+    }
+
+    public static void create() {
+        Sales s1 = new Sales("A038", "黃藥師");
+        Sales s2 = new Sales("C549", "江湖郎中");
+
+        Customer c1 = new Customer("0978978978", "張無忌", s1.getEmNo());
+        Customer c2 = new Customer("0912345678", "趙敏", s1.getEmNo());
+        Customer c3 = new Customer("0911223344", "周芷若", s2.getEmNo());
+
+        Set<Customer> sc1 = new HashSet<Customer>();
+        sc1.add(c1);
+        sc1.add(c2);
+        s1.setCustomers(sc1);
+
+        Set<Customer> sc2 = new HashSet<Customer>();
+        sc2.add(c3);
+        s2.setCustomers(sc2);
+
+        Configuration cfg = new Configuration().configure();
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.getCurrentSession();
+        session.beginTransaction();
+
+        session.save(s1); 
+        session.save(s2);
+
+        session.getTransaction().commit();
+
+    }
+}
