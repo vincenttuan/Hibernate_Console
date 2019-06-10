1e12d6b6e434106c000d9e499a302d262ff8f119
student <student@192.168.2.15>
2019-06-10 21:35:37 +0800

    JavaWeb

---
+++ src/main/java/stock/orm/model/Investor.java
@@ -0,0 +1,41 @@
+package stock.orm.model;
+
+public class Investor {
+    private Integer investorId;
+    private String name;
+    private Double units;
+    private Integer netValue;
+
+    public Integer getInvestorId() {
+        return investorId;
+    }
+
+    public void setInvestorId(Integer investorId) {
+        this.investorId = investorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getUnits() {
+        return units;
+    }
+
+    public void setUnits(Double units) {
+        this.units = units;
+    }
+
+    public Integer getNetValue() {
+        return netValue;
+    }
+
+    public void setNetValue(Integer netValue) {
+        this.netValue = netValue;
+    }
+    
+}
